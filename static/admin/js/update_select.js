8c985bb2a6d18bf48cce3591dd1259873e3c11b8
kuzhaziyat <kuzh.aziyat@gmail.com>
2024-03-07 10:56:48 +0000

    1

--- static/admin/js/update_select.js
+++ static/admin/js/update_select.js
@@ -1,25 +1,23 @@
 const selectOrg = document.getElementById('id_organization');
 const selectDep = document.getElementById('id_department');
-
-selectOrg.addEventListener('change', function(event){
-    const orgId = event.target.value;
-    $.ajax({
-        url: "/task/get_departments/"+orgId+"/", // Предполагается, что у вас есть URL, который возвращает список отделов для данной организации
-        type: "GET",
-        dataType: "json",
-        success: (data) => {
-            // Очищаем текущие варианты отделов
-            selectDep.innerHTML = '';
-            // Добавляем новые варианты отделов на основе полученных данных
-            data.forEach((department) => {
-                const option = document.createElement('option');
-                option.text = department.name; // Предполагается, что у вас есть свойство 'name' для каждого отдела
-                option.value = department.id; // Предполагается, что у вас есть свойство 'id' для каждого отдела
-                selectDep.appendChild(option);
-            });
-        },
-        error: (error) => {
-            console.log(error);
-        }
+$(document).ready(function () {
+    selectOrg.addEventListener('change', function (event) {
+        const orgId = event.target.value;
+        $.ajax({
+            url: '/task/get_departments/' + orgId + '/', // Используйте обратные кавычки (`) для вставки значения переменной
+            type: "GET",
+            dataType: "json",
+            success: (data) => {
+                // Очищаем текущие варианты отделов
+                $('#id_department').empty();
+                // Добавляем новые варианты отделов на основе полученных данных
+                $.each(data, function (index, department) {
+                    $('#id_department').append('<option value="' + department.id + '">' + department.name + '</option>');
+                });
+            },
+            error: (error) => {
+                console.log(error);
+            }
+        });
     });
 });
